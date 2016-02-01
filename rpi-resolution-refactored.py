589e96d268ec1b56ecec7a546fddd9988daeab84
Ap0c <Ap0c@users.noreply.github.com>
2016-02-01 13:19:11 +0000

    Added option to set resolution.

--- rpi-resolution-refactored.py
+++ rpi-resolution-refactored.py
@@ -3,6 +3,17 @@
 import subprocess
 
 
+# ----- Setup ----- #
+
+RES_FILES = {
+	't': 'resolution-options-tv.txt',
+	'm': 'resolution-options-monitor.txt'
+}
+
+TV_MODES = 59
+MON_MODES = 86
+
+
 # ----- Functions ----- #
 
 def save_restart():
@@ -14,13 +25,71 @@ def save_restart():
 	subprocess.call(["reboot"])
 
 
+def get_resolution(screen):
+
+	"""Gets the resolution from the user."""
+
+	print('What resolution would you like?')
+	res = input('Enter an HDMI mode number from the list above: ')
+
+	try:
+		res = int(res)
+	except ValueError:
+		print('Please enter a whole number.')
+		return get_resolution(screen)
+
+	modes = range(1, TV_MODES + 1) if screen is 't' else range(1, MON_MODES + 1)
+
+	if res in modes:
+		return res
+	else:
+		print('No such resolution.')
+		return get_resolution(screen)
+
+
+def get_screen():
+
+	"""Gets the screen type."""
+
+	print('What type of screen are you using?')
+	screen = input("'t' for TV, 'm' for computer monitor: ")
+
+	if screen in ['t', 'm']:
+		return screen
+	else:
+
+		print('No such screen type.')
+		return get_screen()
+
+
+def print_resolutions(screen):
+
+	"""Prints the resolution options for the user."""
+
+	res_file = RES_FILES[screen]
+
+	with open(res_file, 'r') as f:
+		print(f.read())
+
+
+def resolution():
+
+	"""Alters the resolution of the display based on user choice."""
+
+	current_res = subprocess.call(['tvservice', '-s'])
+	print('Your current resolution is: {}\n'.format(current_res))
+
+	screen = get_screen()
+	print_resolutions(screen)
+	res = get_resolution()
+
+
 def perform_action(option):
 
 	"""Runs the option chosen by the user."""
 
 	if option == 'r':
-		# resolution()
-		pass
+		resolution()
 	elif option == 'o':
 		# overscan()
 		pass
@@ -30,9 +99,9 @@ def perform_action(option):
 	elif option == 'q':
 		pass
 	else:
-		return False
+		return 'fail'
 
-	return True
+	return 'success'
 
 
 def user_input():
@@ -47,7 +116,9 @@ def user_input():
 
 	choice = input('What would you like to do: ')
 
-	if perform_action(choice):
+	result = perform_action(choice)
+
+	if result == 'success':
 		print('Done.')
 	else:
 		print('Error: unrecognised option')
